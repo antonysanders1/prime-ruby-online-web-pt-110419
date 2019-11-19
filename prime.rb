781d6a2797bbc302c0b457604de7b9a60db1e01a
Antony Sanders <antony.devon92@gmail.com>
2019-11-19 04:20:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,26 @@
 # Add  code here!
+
+require 'pry'
+
 def prime?(num)
    if num <= 1
       return false 
    elsif num <= 3
       return true
    else (2..num/2).none? do |i|
+     binding.pry
       num % i == 0 
     end
   end
 end
+
+
+=begin
+def prime?(number)
+  if number ==  number.odd? && (number / 2 == Integer)
+    return false
+  else number == number.odd?
+    return true 
+  end
+end
+=end
